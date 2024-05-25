9a946f7163e75cc314ee4a5abf0aa5c49391430f
Michel Jung <michel.jung89@gmail.com>
2024-05-25 12:05:29 +0200

    Try ../pi-gen

--- clean-stage/prerun.sh
+++ clean-stage/prerun.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-
 echo "CURRENT: $(pwd)"
 ls -lah
-ls -lah pi-gen/
-
-# Even though they are not listed in stage-list, they were built.
-touch pi-gen/stage2/SKIP pi-gen/stage2/SKIP_IMAGE
-touch pi-gen/stage3/SKIP pi-gen/stage3/SKIP_IMAGE
-touch pi-gen/stage4/SKIP pi-gen/stage4/SKIP_IMAGE
-touch pi-gen/stage5/SKIP pi-gen/stage5/SKIP_IMAGE
+ls -lah ..
+ls -lah ../pi-gen/
